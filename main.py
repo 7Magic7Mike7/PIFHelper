28f0f4b6d17e70eca3be9dbbc9cb6380b74b9b92
Michael Artner <michael.artner@hotmail.de>
2023-02-27 11:05:30 +0100

    introduced fused evolution lines

--- main.py
+++ main.py
@@ -4,19 +4,79 @@ from sys import argv
 import pkmn_inf_fusion as pif
 
 
-if __name__ == '__main__':
-    base_path = argv[1]
-    dex_names = os.path.join("data", "dex_names.txt")
-    helper = pif.Helper(base_path, dex_names)
+def console_info(base_path: str, helper: pif.Helper, evo_helper: pif.EvolutionHelper):
+    #######################################################################
+    pokemon = "Wynaut"
+    min_rate = 0.4  # how much of the possible fusion evolutions must exist
+    head_details_for = None
+    body_details_for = head_details_for
+    #######################################################################
+    if head_details_for is not None and head_details_for != "None":
+        head_details_for = helper.retriever.get_id(head_details_for)
+    if body_details_for is not None and body_details_for != "None":
+        body_details_for = helper.retriever.get_id(body_details_for)
 
-    id_ = helper.retriever.get_id("Raichu")
-    heads = helper.get_head_fusions(id_)
-    # heads.sort()
-    bodies = helper.get_body_fusions(id_)
-    # bodies.sort()
-    print(f"As head #{len(heads)}:")
-    print(", ".join([f"{body}" for body in heads]))
+    dex_num = helper.retriever.get_id(pokemon)
+    evo_lines = evo_helper.get_evolution_lines(dex_num)
+
+    head_fusions = helper.retriever.get_fusions(base_path, dex_num, as_head=True, as_names=False)
+    head_fusions.sort()
+    head_fusions = evo_helper.dex_nums_to_evo_lines(head_fusions)
+    body_fusions = helper.retriever.get_fusions(base_path, dex_num, as_head=False, as_names=False)
+    body_fusions.sort()
+    body_fusions = evo_helper.dex_nums_to_evo_lines(body_fusions)
+
+    print("########################################")
+    print("################  HEAD  ################")
+    print("########################################")
     print()
-    print(f"As body #{len(bodies)}:")
-    print(", ".join([f"{head}" for head in bodies]))
 
+    counter = 0
+    for evo_line in evo_lines:
+        for other_line in head_fusions:
+            fused_line = pif.FusedEvoLine(base_path, helper.retriever, evo_line, other_line, unidirectional=True)
+            if fused_line.has_final and fused_line.rate >= min_rate:
+                if head_details_for is None:
+                    print(fused_line.formatted_string(existing=False, missing=False, headline_ids=False))
+                    counter += 1
+                    if counter % 5 == 0:
+                        print()
+                elif head_details_for in other_line:
+                    print(fused_line.formatted_string(missing=False, headline_ids=False))
+                    counter += 1
+                    if counter % 5 == 0:
+                        print()
+        print()
+
+    print()
+    print("########################################")
+    print("################  BODY  ################")
+    print("########################################")
+    print()
+
+    counter = 0
+    for evo_line in evo_lines:
+        for other_line in body_fusions:
+            fused_line = pif.FusedEvoLine(base_path, helper.retriever, other_line, evo_line, unidirectional=True)
+            if fused_line.has_final and fused_line.rate >= min_rate:
+                if body_details_for is None:
+                    print(fused_line.formatted_string(existing=False, missing=False, headline_ids=False))
+                    counter += 1
+                    if counter % 5 == 0:
+                        print()
+
+                elif body_details_for in other_line:
+                    print(fused_line.formatted_string(missing=False, headline_ids=False))
+                    counter += 1
+                    if counter % 5 == 0:
+                        print()
+        print()
+
+
+if __name__ == '__main__':
+    base_path_ = argv[1]
+
+    helper_ = pif.Helper(base_path_, os.path.join("data", "dex_names.txt"))
+    evo_helper_ = pif.EvolutionHelper(os.path.join("data", "evolutions.txt"))
+
+    console_info(base_path_, helper_, evo_helper_)
--- pkmn_inf_fusion/evolution_helper.py
+++ pkmn_inf_fusion/evolution_helper.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Optional, Union, Tuple
+from typing import List, Dict, Optional, Union, Tuple, Set
 
 from pkmn_inf_fusion import util, FusionRetriever
 
@@ -58,6 +58,15 @@ class EvolutionLine:
         return None
 
     @property
+    def end_stage(self) -> int:
+        if self.evo1 is None:
+            return self.base
+        elif self.evo2 is None:
+            return self.evo1
+        else:
+            return self.evo2
+
+    @property
     def is_multi_stage(self) -> bool:
         return self.size > 1
 
@@ -70,6 +79,14 @@ class EvolutionLine:
         for evo in self.__evos:
             text += f" -> {fusion_retriever.get_name(evo)}"
         return text
+
+    def to_list(self) -> List[int]:
+        list_ = [self.base]
+        if self.evo1 is not None:
+            list_.append(self.evo1)
+        if self.evo2 is not None:
+            list_.append(self.evo2)
+        return list_
 
     def __contains__(self, item):
         if isinstance(item, int):
@@ -84,6 +101,18 @@ class EvolutionLine:
         for evo in self.__evos:
             text += f" -> {evo}"
         return text
+
+    def __hash__(self):
+        val = 17 * self.base
+        if self.evo1 is not None:
+            val += 311 * self.evo1
+        if self.evo2 is not None:
+            val += 523 * self.evo2
+        return val
+
+    def __eq__(self, other):
+        if isinstance(other, EvolutionLine):
+            return self.base == other.base and self.evo1 == other.evo1 and self.evo2 == other.evo2
 
 
 class EvolutionHelper:
@@ -131,3 +160,77 @@ class EvolutionHelper:
                 self.__evolutions[dex_num]
             ))
         return self.__evolutions[dex_num]
+
+    def dex_nums_to_evo_lines(self, dex_nums: List[int]) -> List[EvolutionLine]:
+        evo_lines: Set[EvolutionLine] = set()
+
+        for dex_num in dex_nums:
+            cur_lines = self.get_evolution_lines(dex_num, exclude_parallels=True)
+            for line in cur_lines:
+                evo_lines.add(line)
+
+        return list(evo_lines)
+
+
+class FusedEvoLine:
+    def __init__(self, base_path: str, retriever: FusionRetriever, evo_line1: EvolutionLine, evo_line2: EvolutionLine,
+                 unidirectional: bool = False):
+        self.__retriever = retriever
+        self.__line1 = evo_line1
+        self.__line2 = evo_line2
+
+        existing: List[Tuple[int, int]] = []
+        missing: List[Tuple[int, int]] = []
+
+        def check_line(line1: EvolutionLine, line2: EvolutionLine):
+            for dex_num in line1.to_list():
+                fusions = retriever.get_fusions(base_path, dex_num, as_head=True, as_names=False)
+                for fused_num in line2.to_list():
+                    if fused_num in fusions:
+                        existing.append((dex_num, fused_num))
+                    else:
+                        missing.append((dex_num, fused_num))
+
+        check_line(evo_line1, evo_line2)
+        if not unidirectional:
+            check_line(evo_line2, evo_line1)
+
+        self.__existing = existing
+        self.__missing = missing
+
+    @property
+    def rate(self) -> float:
+        return len(self.__existing) / (len(self.__existing) + len(self.__missing))
+
+    @property
+    def has_final(self) -> bool:
+        return (self.__line1.end_stage, self.__line2.end_stage) in self.__existing
+
+    def formatted_string(self, existing: bool = True, missing: bool = True, headline_ids: bool = True) -> str:
+        text = "" #f"Fusions of Evolution lines "
+        if headline_ids:
+            text += f"#{self.__line1.end_stage} & " \
+                    f"#{self.__line2.end_stage}"
+        else:
+            text += f"{self.__retriever.get_name(self.__line1.end_stage)} & " \
+                    f"{self.__retriever.get_name(self.__line2.end_stage)}"
+
+        if existing and len(self.__existing) > 0:
+            text += "\n\tExisting:\n"
+            for fusion in self.__existing:
+                head, body = fusion
+                text += f"\t\t- {self.__retriever.get_name(head)} + {self.__retriever.get_name(body)}\n"
+            text = text[:-1]    # remove last \n
+
+        if missing and len(self.__missing) > 0:
+            text += "\n\tMissing:\n"
+            for fusion in self.__missing:
+                head, body = fusion
+                text += f"\t\t- {self.__retriever.get_name(head)} + {self.__retriever.get_name(body)}\n"
+            text = text[:-1]    # remove last \n
+
+        return text
+
+    def __str__(self) -> str:
+        return f"EvoLine-Fusions of #{self.__line1.end_stage} & #{self.__line2.end_stage} " \
+               f"({len(self.__existing)} exist, {len(self.__missing)} miss)"
