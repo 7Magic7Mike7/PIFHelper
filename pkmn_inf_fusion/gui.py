f238daa691dcf0194cc3ceb1e7fa872efc770d45
Michael Artner <michael.artner@hotmail.de>
2023-10-23 19:22:12 +0200

    - added filter area to filter results by certain stats - added drawing of UI - rate is now in % - available mons is now Entry (only 1 line)

--- pkmn_inf_fusion/gui.py
+++ pkmn_inf_fusion/gui.py
@@ -1,13 +1,62 @@
 import tkinter
-from tkinter import ttk, Tk, IntVar, StringVar
-from typing import List, Tuple, Set, Optional
+from tkinter import ttk, Tk, IntVar, StringVar, DoubleVar
+from typing import List, Tuple, Set, Optional, Dict
 
 from pkmn_inf_fusion import EvolutionHelper, FusionRetriever, EvolutionLine, FusedEvoLine, util
 
 
 class GUI:
+    class _Filter:
+        def __init__(self):
+            self.__vars: Dict[str, Tuple[DoubleVar, StringVar, ttk.Label, ttk.Entry, ttk.Scale]] = {}
+
+        def add_filter(self, master, name: str, from_: float, to_: float, label: Optional[str] = None,
+                       col: Optional[int] = None, row: Optional[int] = None) -> Tuple[ttk.Label, ttk.Entry, ttk.Scale]:
+            assert name not in self.__vars, f"Name={name} already used!"
+
+            if label is None: label = name
+
+            double_var = DoubleVar(value=0)
+            string_var = StringVar(value="0")
+
+            label_ = ttk.Label(master, text=label)
+
+            entry_width = max(len(str(from_)), len(str(to_)))
+            entry = ttk.Entry(master, width=entry_width, textvariable=string_var)
+            entry.bind("<Key-Return>", lambda event: self._set_scale(name))
+
+            scale = ttk.Scale(master, from_=from_, to=to_, variable=double_var,
+                              command=lambda event: self._set_text(name))
+
+            if col is None:
+                label_.grid(row=row)
+                entry.grid(row=row)
+                scale.grid(row=row)
+            else:
+                label_.grid(column=col, row=row)
+                entry.grid(column=col+1, row=row)
+                scale.grid(column=col+2, row=row)
+
+            self.__vars[name] = double_var, string_var, label_, entry, scale
+            return label_, entry, scale
+
+        def _set_text(self, stat: str):
+            assert stat in self.__vars, f"Variable={stat} not stored in this filter!"
+
+            double_var, string_var, _, _, _ = self.__vars[stat]
+            if string_var is not None and double_var is not None:
+                string_var.set(f"{double_var.get():.0f}")
+
+        def _set_scale(self, stat: str):
+            assert stat in self.__vars, f"Variable={stat} not stored in this filter!"
+
+            double_var, string_var, _, _, scale = self.__vars[stat]
+            if string_var is not None and double_var is not None:
+                val = float(string_var.get())
+                double_var.set(val)     # todo check if val is valid/in range?
+
     __MON_SPLITER = ";"
-    __DEFAULT_RATE = 0.5
+    __DEFAULT_RATE = 50
     __SM_NAME = 0
     __SM_RATE = 1
     __SM_DEX = 2
@@ -21,30 +70,77 @@ class GUI:
         self.__pokemon_list = retriever.get_all_names()
         self.__analyzed_mons: Set[int] = set()
 
+        # rough looks of the UI
+        """
+        Columns
+        0     1     2          3      |    4                    5                 6         7      8
+        ###########################################################################################################
+        Main Pokemon: [________|v]         Available Pokemon: [_______________]
+        Filter:                            Results:                               Selected Fusion Data:
+        BST   ? -------------              -----------------------------------    -----------------------
+        HP    ? -------------              |~ Head                           |    | BST:    123         |
+        ATK   ? -------------              | + EvoLine1 #id xNumFusions      |    | HP:     123         |
+        SPATK ? -------------              | ~ EvoLine2 #id xNumFusions      |    | ATK:    123         |
+        DEF   ? -------------              |    + FusedLine1 #id %completion |    | DEF:    123         |
+        SPDEF ? -------------              |    ~ FusedLine2 #id %completion |    | SPATK:  123         |
+        SPD   ? -------------              |         Fusion1                 |    | SPDEF:  124         |
+                                           |         Fusion2                 |    | SPD:    123         |
+        TYPE(s) [____|v] OR/AND [____|v]   |         Fusion3                 |    |                     |
+        Min rate 0 ---------- 100          |                                 |    | Type:    T1  (T2)   |
+        Ability [____|v]                   |+ Body                           |    | Abilities:  [____|v]|
+        Move    [____|v]                   |                                 |    | Moves?              |
+        Only BST > BaseMons [x]            -----------------------------------    -----------------------
+        
+                {ANALYZE}                                                         {OPEN IMAGE OF SELECTED FUSION}
+        """
+
+        # title
         self.__root.title("Pokemon Infinite Fusion - Fusion Checker")
-        row = 0
-        frm = ttk.Frame(self.__root, width=200, height=200, padding=10)
+        frm = ttk.Frame(self.__root, width=500, height=300, padding=10)
         frm.grid()
 
-        ttk.Label(frm, text="Main Pokemon: ").grid(column=0, row=row)
+        # styles
+        stypeHL = ttk.Style()   # headlines
+        stypeHL.configure("BW.TLabel", foreground="red", background="black")
+
+        # ROW 0
+        # main pokemon to search for fusions
+        ttk.Label(frm, text="Main Pokemon: ").grid(column=0, row=0)
         self.__e_main_mon = ttk.Combobox(frm, values=self.__pokemon_list)
-        self.__e_main_mon.grid(column=1, row=row, columnspan=1)
-        row += 1
+        self.__e_main_mon.grid(column=1, row=0, columnspan=1)
 
-        ttk.Label(frm, text="Available Pokemon: ").grid(column=0, row=row)
-        row += 1
-        self.__available_mons = tkinter.Text(frm, width=20, height=15)
-        self.__available_mons.grid(column=0, row=row, columnspan=2)
-        row += 1
+        # available pokemon to fuse with main pokemon
+        ttk.Label(frm, text="Available Pokemon: ").grid(column=4, row=0)
+        self.__available_mons = ttk.Entry(frm, width=20)
+        self.__available_mons.grid(column=5, row=0, columnspan=1)
 
-        ttk.Label(frm, text="Min rate: ").grid(column=0, row=row)
-        self.__e_rate = ttk.Entry(frm, width=5)
+        # ROW 1 separator
+        ttk.Separator(frm, orient="horizontal").grid(column=0, row=1, columnspan=2)     # todo not working
+
+        # Filter section
+        row = 2
+        ttk.Label(frm, text="Filter: ", style="BW.TLabel").grid(column=0, row=row)
+        self.__filter = GUI._Filter()
+
+        self.__filter.add_filter(frm, "bst", from_=0, to_=800, label="BST  ", col=0, row=row+1)
+
+        self.__filter.add_filter(frm, "hp", from_=0, to_=600, label="HP   ", col=0, row=row+2)
+        self.__filter.add_filter(frm, "atk", from_=0, to_=600, label="ATK  ", col=0, row=row+3)
+        self.__filter.add_filter(frm, "spatk", from_=0, to_=600, label="SPATK", col=0, row=row+4)
+        self.__filter.add_filter(frm, "def", from_=0, to_=600, label="DEF  ", col=0, row=row+5)
+        self.__filter.add_filter(frm, "spdef", from_=0, to_=600, label="SPDEF", col=0, row=row+6)
+        self.__filter.add_filter(frm, "spd", from_=0, to_=600, label="SPD  ", col=0, row=row+7)
+
+        # minimum completion rate of main pokemon's and other pokemon's evolution line
+        row = row+10
+        ttk.Label(frm, text="Min % ").grid(column=0, row=row)
+        self.__e_rate = ttk.Entry(frm, width=3)
         self.__e_rate.grid(column=1, row=row)
-        self.__e_rate.insert(0, f"{GUI.__DEFAULT_RATE}")
-        row += 1
+        self.__e_rate.insert(0, f"{GUI.__DEFAULT_RATE}")    # todo validate input?
 
-        ttk.Button(frm, text="Analyse", command=self.__analyse, width=30).grid(column=0, row=row, columnspan=2)
-        ttk.Button(frm, text="Reset", command=self.__reset, width=30).grid(column=2, row=row)
+        # Result section
+        row = 2
+        ttk.Label(frm, text="Results: ").grid(column=4, row=row)
 
         ttk.Label(frm, text="Head Fusions: ").grid(column=2, row=1)
         self.__tree_head_fusions = ttk.Treeview(frm, columns=("fusions"))
@@ -54,6 +150,17 @@ class GUI:
         self.__tree_body_fusions = ttk.Treeview(frm, columns=("fusions"))
         self.__tree_body_fusions.grid(column=3, row=2)
 
+        # Fusion details section
+        row = 2
+        ttk.Label(frm, text="Selected Fusion Data: ").grid(column=6, row=row)
+
+
+        # buttons at the bottom
+        row = 16
+        ttk.Button(frm, text="Analyse", command=self.__analyse, width=30).grid(column=0, row=row, columnspan=2)
+        ttk.Button(frm, text="Reset", command=self.__reset, width=30).grid(column=2, row=row)
+
+        # insert values
         if main_mon is not None:
             self.__e_main_mon.insert(0, main_mon)
         if available_mons is not None:
@@ -68,11 +175,9 @@ class GUI:
     def _filter_by_availability(self, evolution_lines: List[EvolutionLine]) -> List[EvolutionLine]:
         available_ids = []
         # go through every line in our text area for available pokemon
-        for line in self.__available_mons.get("1.0", "end").split("\n"):
-            # there can be multiple pokemon per line
-            for mon in line.split(GUI.__MON_SPLITER):
-                id_ = self.__retriever.get_id(mon)
-                if id_ != -1: available_ids.append(id_)
+        for mon in self.__available_mons.get().split(";"):
+            id_ = self.__retriever.get_id(mon)
+            if id_ != -1: available_ids.append(id_)
 
         if len(available_ids) <= 0: return evolution_lines  # don't filter if we would get an empty result
         return EvolutionHelper.availability_filter(evolution_lines, available_ids)
@@ -80,7 +185,7 @@ class GUI:
     def __analyse(self):
         # validate user input
         assert self.__retriever.get_id(self.__e_main_mon.get()) >= 0, f"Entry not valid: {self.__e_main_mon.get()}!"
-        min_rate = float(self.__e_rate.get())
+        min_rate = float(self.__e_rate.get()) / 100     # convert % to decimal
         assert 0 <= min_rate <= 1.0, f"Invalid rate: {min_rate}!"
 
         self.__reset()      # remove old data before we analyse
